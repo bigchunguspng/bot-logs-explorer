800e7ea4984a0ece698544fe502494c90b0ae925
bigchungus <70759405+bigchunguspng@users.noreply.github.com>
2025-05-06 11:08:11 +0300

    Numero Uno

---
+++ Program.cs
@@ -0,0 +1,79 @@
+﻿using System.Text.RegularExpressions;
+using CommandLine;
+
+namespace BotLogsExplorer
+{
+    public class Options
+    {
+        [Option('i', "include", Required = false)]
+        public string? Include { get; set; }
+
+        [Option('e', "exclude", Required = false)]
+        public string? Exclude { get; set; }
+
+        [Option('r', "ignore", Required = false)]
+        public string? Ignore { get; set; }
+
+        [Option('g', "group", Required = false, Default = -1)]
+        public int Group { get; set; }
+
+        [Value(0, Default = "log*.txt", MetaName = "FILE")]
+        public string FilePath { get; set; } = null!;
+    }
+
+    internal static class Program
+    {
+        static void Main(string[] args)
+        {
+            var options = Parser.Default.ParseArguments<Options>(args).Value;
+
+            var lines = File.ReadAllLines(options.FilePath);
+            Console.WriteLine($"{lines.Length, 8} - LINES TOTAL");
+
+            var linesQuery = lines.AsEnumerable();
+            if (options.Include != null)
+            {
+                var regex = new Regex(options.Include);
+                linesQuery = linesQuery.Where(x => regex.IsMatch(x));
+            }
+            if (options.Exclude != null)
+            {
+                var regex = new Regex(options.Exclude);
+                linesQuery = linesQuery.Where(x => !regex.IsMatch(x));
+            }
+            if (options.Ignore != null)
+            {
+                var ignore = options.Ignore;
+                linesQuery = linesQuery.Select(x => x.Replace(ignore, ""));
+            }
+
+            var linesFiltered = linesQuery.ToList();
+            var countFiltered = linesFiltered.Count;
+            Console.WriteLine($"{countFiltered, 8} - LINES FILTERED");
+
+            if (options is { Include: not null, Group: > 0 })
+            {
+                var regex = new Regex(options.Include);
+                var groups = linesFiltered
+                    .GroupBy(x => regex.Match(x).Groups[options.Group].Value)
+                    .OrderByDescending(x => x.Count()).ToList();
+
+                Console.WriteLine($"{groups.Count, 8} - LINES DISTINCT");
+                Console.WriteLine("\n   COUNT PERCENT EVENT");
+                foreach (var group in groups)
+                {
+                    var count = group.Count();
+                    var percent = Math.Round(100F * count / countFiltered, 2);
+                    Console.WriteLine($"{count,8} {percent,6}% {group.Key}");
+                }
+            }
+            else
+            {
+                foreach (var line in linesFiltered)
+                {
+                    Console.WriteLine(line);
+                }
+            }
+        }
+    }
+}
