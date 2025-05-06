8d9c34a523db68162cf696429d893b563e45b259
bigchungus <70759405+bigchunguspng@users.noreply.github.com>
2025-05-06 12:00:58 +0300

    Help texts, --chat, --command

--- Program.cs
+++ Program.cs
@@ -5,27 +5,38 @@ namespace BotLogsExplorer
 {
     public class Options
     {
-        [Option('i', "include", Required = false)]
+        [Option('i', "include", Required = false, HelpText = "Filter logs that INCLUDE pattern.")]
         public string? Include { get; set; }
 
-        [Option('e', "exclude", Required = false)]
+        [Option('e', "exclude", Required = false, HelpText = "Filter logs that EXCLUDE pattern.")]
         public string? Exclude { get; set; }
 
-        [Option('r', "ignore", Required = false)]
+        [Option('r', "ignore", Required = false, HelpText = "Text that should be ignored. E.g. @bot_username.")]
         public string? Ignore { get; set; }
 
-        [Option('g', "group", Required = false, Default = -1)]
+        [Option('g', "group", Required = false, Default = -1, HelpText = "Group logs by an index of --include regex capture group.")]
         public int Group { get; set; }
 
-        [Value(0, Default = "log*.txt", MetaName = "FILE")]
+        [Value(0, Default = "log*.txt", MetaName = "FILE", HelpText = "Logs file path.")]
         public string FilePath { get; set; } = null!;
+
+        [Option('c', "chat", Required = false, Default = -1, HelpText = "Filter logs by chat id (last 4 numbers).")]
+        public int Chat { get; set; }
+        
+        [Option('m', "command", Required = false, HelpText = "Filter logs by command (without slash).")]
+        public string? Command { get; set; }
     }
 
     internal static class Program
     {
+        public const string AUTO = @"(\[auto,\s*(\S*)\]\s)?";
+
         static void Main(string[] args)
         {
             var options = Parser.Default.ParseArguments<Options>(args).Value;
+            if (options.Chat > 0 && options.Command != null) options.Include = $@"{options.Chat}] >> {AUTO}((\/{options.Command}\S*)(?:\s(.*))?)";
+            else if (options.Command != null)                options.Include =               $@"] >> {AUTO}((\/{options.Command}\S*)(?:\s(.*))?)";
+            else if (options.Chat > 0)                       options.Include = $@"{options.Chat}] >> {AUTO}((\S*).*)";
 
             var lines = File.ReadAllLines(options.FilePath);
             Console.WriteLine($"{lines.Length, 8} - LINES TOTAL");
