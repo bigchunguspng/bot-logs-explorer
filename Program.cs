30d64c1f678eda3ecc1a6713645d0848c2fc563f
bigchungus <70759405+bigchunguspng@users.noreply.github.com>
2025-05-06 13:00:09 +0300

    --debug, --limit, --ignore*

--- Program.cs
+++ Program.cs
@@ -11,7 +11,7 @@ namespace BotLogsExplorer
         [Option('e', "exclude", Required = false, HelpText = "Filter logs that EXCLUDE pattern.")]
         public string? Exclude { get; set; }
 
-        [Option('r', "ignore", Required = false, HelpText = "Text that should be ignored. E.g. @bot_username.")]
+        [Option('r', "ignore", Required = false, HelpText = "Pattern that should be removed, e.g. \"@xyz_bot(?<=\\S)\".")]
         public string? Ignore { get; set; }
 
         [Option('g', "group", Required = false, Default = -1, HelpText = "Group logs by an index of --include regex capture group.")]
@@ -25,6 +25,12 @@ namespace BotLogsExplorer
         
         [Option('m', "command", Required = false, HelpText = "Filter logs by command (without slash).")]
         public string? Command { get; set; }
+
+        [Option('d', "debug", Required = false)]
+        public bool Debug { get; set; }
+
+        [Option('l', "limit", Required = false, Default = -1, HelpText = "Limit output to a max lines value.")]
+        public int Limit { get; set; }
     }
 
     internal static class Program
@@ -37,6 +43,18 @@ namespace BotLogsExplorer
             if (options.Chat > 0 && options.Command != null) options.Include = $@"{options.Chat}] >> {AUTO}((\/{options.Command}\S*)(?:\s(.*))?)";
             else if (options.Command != null)                options.Include =               $@"] >> {AUTO}((\/{options.Command}\S*)(?:\s(.*))?)";
             else if (options.Chat > 0)                       options.Include = $@"{options.Chat}] >> {AUTO}((\S*).*)";
+
+            if (options.Limit < 0)
+            {
+                options.Limit = int.MaxValue;
+            }
+
+            if (options.Debug)
+            {
+                Console.WriteLine($"INCLUDE: {options.Include}");
+                Console.WriteLine($"EXCLUDE: {options.Exclude}");
+                Console.WriteLine();
+            }
 
             var lines = File.ReadAllLines(options.FilePath);
             Console.WriteLine($"{lines.Length, 8} - LINES TOTAL");
@@ -54,8 +72,8 @@ namespace BotLogsExplorer
             }
             if (options.Ignore != null)
             {
-                var ignore = options.Ignore;
-                linesQuery = linesQuery.Select(x => x.Replace(ignore, ""));
+                var regex = new Regex(options.Ignore);
+                linesQuery = linesQuery.Select(x => regex.Replace(x, ""));
             }
 
             var linesFiltered = linesQuery.ToList();
@@ -71,7 +89,7 @@ namespace BotLogsExplorer
 
                 Console.WriteLine($"{groups.Count, 8} - LINES DISTINCT");
                 Console.WriteLine("\n   COUNT PERCENT EVENT");
-                foreach (var group in groups)
+                foreach (var group in groups.Take(options.Limit))
                 {
                     var count = group.Count();
                     var percent = Math.Round(100F * count / countFiltered, 2);
@@ -80,7 +98,8 @@ namespace BotLogsExplorer
             }
             else
             {
-                foreach (var line in linesFiltered)
+                Console.WriteLine();
+                foreach (var line in linesFiltered.Take(options.Limit))
                 {
                     Console.WriteLine(line);
                 }
