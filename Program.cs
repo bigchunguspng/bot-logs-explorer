186fc0547f26c55bec38baa3393f5e7c1eaa4332
bigchungus <70759405+bigchunguspng@users.noreply.github.com>
2026-04-11 15:25:01 +0300

    #

--- Program.cs
+++ Program.cs
@@ -1,88 +1,42 @@
 ﻿using System.Globalization;
 using System.Text.RegularExpressions;
+using BotLogsExplorer;
 using CommandLine;
 using CommandLine.Text;
 
-namespace BotLogsExplorer;
+var parser = new Parser(with => with.HelpWriter = null);
+var result = parser.ParseArguments<Options>(args);
+_ = result
+    .WithParsed(Run)
+    .WithNotParsed(_ => DisplayHelp());
 
-public class Options
+
+void DisplayHelp()
 {
-    [Value(0, Default = "./Log/log.txt", MetaName = "FILE", HelpText = "Logs file path.")]
-    public string FilePath { get; set; } = null!;
-
-    [Option('i', "include", HelpText = "Filter lines with given pattern.")]
-    public string? Include { get; set; }
-
-    [Option('e', "exclude", HelpText = "Filter lines w/o  given pattern.")]
-    public string? Exclude { get; set; }
-
-    [Option('r', "remove", HelpText = "Pattern to remove from lines, e.g. \"@xyz_bot(?<=\\S)\".")]
-    public string? Remove { get; set; }
-
-    [Option('g', "group", Default = -1, HelpText = "Group lines by regex group index of --include pattern.")]
-    public int GroupIndex { get; set; }
-
-    [Option('G', "filter-group", HelpText = "Filter lines by value of regex group supplied via --group.")]
-    public string? GroupValue { get; set; }
-
-    [Option('c', "chat", Default = -1, HelpText = "Filter lines by chat id (last 1-5 digits).")]
-    public int Chat { get; set; }
-
-    [Option('C', "command", HelpText = "Filter lines by command (w/o slash).")]
-    public string? Command { get; set; }
-
-    [Option('T', "type", HelpText = "Filter lines by type: [C]OMMAND, [A]UTO, CALL[B]ACK, [I]NLINE, [E]VENT.")]
-    public string? Type { get; set; }
-
-    [Option('E', "event", HelpText = "Filter lines by event: START, SAVE, ADMIN, EXIT etc.")]
-    public string? Event { get; set; }
-
-    [Option('d', "debug", HelpText = "Debug --include and --exculde patterns.")]
-    public bool Debug { get; set; }
-
-    [Option('l', "limit", Default = -1, HelpText = "Max number of lines to output.")]
-    public int Limit { get; set; }
-
-    [Option('t', "time", HelpText = "Visualize data as a timetable.")]
-    public bool Timetable { get; set; }
-
-    [Option('o', "time-offset", HelpText = "Time offset in hours relative to bot timezone.")]
-    public int TimeOffset { get; set; }
+    var helpText = HelpText.AutoBuild(result, help =>
+    {
+        help.Copyright = string.Empty;
+        help.AdditionalNewLineAfterOption = false;
+        help.MaximumDisplayWidth = 120;
+        return HelpText.DefaultParsingErrorsHandler(result, help);
+    });
+    Console.WriteLine(helpText);
 }
 
-internal static class Program
+void Run(Options options)
 {
-    private static void Main(string[] args)
+    // CHECK FILE
+    if (File.Exists(options.FilePath) == false)
     {
-        var parser = new Parser(with => with.HelpWriter = null);
-        var result = parser.ParseArguments<Options>(args);
-        result
-            .WithParsed(Run)
-            .WithNotParsed(_ => DisplayHelp(result));
+        Console.WriteLine($"FILE {options.FilePath} NOT FOUND!");
+        return;
     }
 
-    private static void DisplayHelp<T>(ParserResult<T> result)
+    // ADJUST OPTIONS
     {
-        var helpText = HelpText.AutoBuild(result, help =>
-        {
-            help.Copyright = string.Empty;
-            help.AdditionalNewLineAfterOption = false;
-            help.MaximumDisplayWidth = 120;
-            return HelpText.DefaultParsingErrorsHandler(result, help);
-        });
-        Console.WriteLine(helpText);
-    }
+        if (options.Limit < 0)
+            options.Limit = int.MaxValue;
 
-    private static void Run(Options options)
-    {
-        // CHECK FILE
-        if (File.Exists(options.FilePath) == false)
-        {
-            Console.WriteLine($"FILE {options.FilePath} NOT FOUND!");
-            return;
-        }
-
-        // ADJUST OPTIONS
         if (options.Event != null)
         {
             options.Include = $@" \| .*{options.Event} .. (.*)";
@@ -126,24 +80,23 @@ internal static class Program
                 (false, false) => options.Include,
             };
         }
+    }
 
-        if (options.Limit < 0)
-            options.Limit = int.MaxValue;
+    // DEBUG PATTERNS
+    if (options.Debug)
+    {
+        Console.WriteLine($"INCLUDE: {options.Include}");
+        Console.WriteLine($"EXCLUDE: {options.Exclude}");
+        Console.WriteLine();
+    }
 
-        // DEBUG
-        if (options.Debug)
-        {
-            Console.WriteLine($"INCLUDE: {options.Include}");
-            Console.WriteLine($"EXCLUDE: {options.Exclude}");
-            Console.WriteLine();
-        }
+    // READ FILE
+    var lines = File.ReadAllLines(options.FilePath);
+    Console.WriteLine($"{lines.Length, 8} - LINES TOTAL");
 
-        // READ FILE
-        var lines = File.ReadAllLines(options.FilePath);
-        Console.WriteLine($"{lines.Length, 8} - LINES TOTAL");
-
-        // FILTER LINES
-        var linesQuery = lines.AsEnumerable();
+    // FILTER LINES
+    var linesQuery = lines.AsEnumerable();
+    {
         if (options.Include != null)
         {
             var regex = new Regex(options.Include);
@@ -155,82 +108,84 @@ internal static class Program
                 linesQuery = linesQuery.Where(x => regex_2.IsMatch(regex.Match(x).Groups[options.GroupIndex].Value));
             }
         }
+
         if (options.Exclude != null)
         {
             var regex = new Regex(options.Exclude);
             linesQuery = linesQuery.Where(x => !regex.IsMatch(x));
         }
+
         if (options.Remove != null)
         {
             var regex = new Regex(options.Remove);
             linesQuery = linesQuery.Select(x => regex.Replace(x, ""));
         }
+    }
 
-        var linesFiltered = linesQuery.ToList();
-        var countFiltered = linesFiltered.Count;
-        Console.WriteLine($"{countFiltered, 8} - LINES FILTERED");
+    var linesFiltered = linesQuery.ToList();
+    var countFiltered = linesFiltered.Count;
+    Console.WriteLine($"{countFiltered, 8} - LINES FILTERED");
 
-        // OUTPUT
-        if (options is { Include: not null, GroupIndex: > 0, GroupValue: null })
+    // OUTPUT
+    if (options is { Include: not null, GroupIndex: > 0, GroupValue: null })
+    {
+        var regex = new Regex(options.Include);
+        var groups = linesFiltered
+            .GroupBy(x => regex.Match(x).Groups[options.GroupIndex].Value)
+            .OrderByDescending(x => x.Count()).ToList();
+
+        Console.WriteLine($"{groups.Count, 8} - LINES DISTINCT");
+        Console.WriteLine("\n   COUNT PERCENT EVENT");
+        foreach (var group in groups.Take(options.Limit))
         {
-            var regex = new Regex(options.Include);
-            var groups = linesFiltered
-                .GroupBy(x => regex.Match(x).Groups[options.GroupIndex].Value)
-                .OrderByDescending(x => x.Count()).ToList();
-
-            Console.WriteLine($"{groups.Count, 8} - LINES DISTINCT");
-            Console.WriteLine("\n   COUNT PERCENT EVENT");
-            foreach (var group in groups.Take(options.Limit))
-            {
-                var count = group.Count();
-                var percent = Math.Round(100F * count / countFiltered, 2);
-                Console.WriteLine($"{count,8} {percent,6}% {group.Key}");
-            }
+            var count = group.Count();
+            var percent = Math.Round(100F * count / countFiltered, 2);
+            Console.WriteLine($"{count,8} {percent,6}% {group.Key}");
         }
-        else if (options.Timetable)
+    }
+    else if (options.TimeTable)
+    {
+        var regex = new Regex(@"^(.+?) \| ");
+        var dateTimes = linesFiltered.Select(x =>
         {
-            var regex = new Regex(@"^(.+?) \| ");
-            var dateTimes = linesFiltered.Select(x =>
-            {
-                var time = regex.Match(x).Groups[1].Value;
-                return DateTime.ParseExact(time, "MM/dd HH:mm:ss.fff", CultureInfo.InvariantCulture);
-            }).ToList();
-            var byH = dateTimes.GroupBy(x => x.Hour     ).ToDictionary(x => x.Key, x => x.Count());
-            var byD = dateTimes.GroupBy(x => x.DayOfWeek).ToDictionary(x => x.Key, x => x.Count());
-            var byM = dateTimes.GroupBy(x => x.Month    ).ToDictionary(x => x.Key, x => x.Count());
+            var time = regex.Match(x).Groups[1].Value;
+            return DateTime.ParseExact(time, "MM/dd HH:mm:ss.fff", CultureInfo.InvariantCulture);
+        }).ToList();
+        var byH = dateTimes.GroupBy(x => x.Hour     ).ToDictionary(x => x.Key, x => x.Count());
+        var byD = dateTimes.GroupBy(x => x.DayOfWeek).ToDictionary(x => x.Key, x => x.Count());
+        var byM = dateTimes.GroupBy(x => x.Month    ).ToDictionary(x => x.Key, x => x.Count());
 
-            var k = 100F / countFiltered;
+        var k = 100F / countFiltered;
 
-            Console.WriteLine("\nBY HOUR");
-            for (var i = 0; i < 24; i++)
-            {
-                var x = byH.GetValueOrDefault(i, 0);
-                var hour = (24 + i + options.TimeOffset) % 24;
-                Console.WriteLine($"{hour,2}:00 - {hour + 1,2}:00 {x,8} {new string('=', (int)(k * x))}");
-            }
-
-            Console.WriteLine("\nBY WEEK DAY");
-            for (var i = 0; i < 7; i++)
-            {
-                var x = byD.GetValueOrDefault((DayOfWeek)i, 0);
-                Console.WriteLine($"{(DayOfWeek)i,13} {x,8} {new string('=', (int)(k * x))}");
-            }
-
-            Console.WriteLine("\nBY MONTH");
-            for (var i = 1; i < 13; i++)
-            {
-                var x = byM.GetValueOrDefault(i, 0);
-                var month = CultureInfo.InvariantCulture.DateTimeFormat.GetMonthName(i);
-                Console.WriteLine($"{month,13} {x,8} {new string('=', (int)(k * x))}");
-            }
+        Console.WriteLine("\nBY HOUR");
+        for (var i = 0; i < 24; i++)
+        {
+            var x = byH.GetValueOrDefault(i, 0);
+            var hour = (24 + i + options.TimeOffset) % 24;
+            Console.WriteLine($"{hour,2}:00 - {hour + 1,2}:00 {x,8} {new string('=', (int)(k * x))}");
         }
-        else
+
+        Console.WriteLine("\nBY WEEK DAY");
+        for (var i = 0; i < 7; i++)
         {
-            Console.WriteLine();
-            foreach (var line in linesFiltered.Take(options.Limit))
-            {
-                Console.WriteLine(line);
-            }
+            var x = byD.GetValueOrDefault((DayOfWeek)i, 0);
+            Console.WriteLine($"{(DayOfWeek)i,13} {x,8} {new string('=', (int)(k * x))}");
+        }
+
+        Console.WriteLine("\nBY MONTH");
+        for (var i = 1; i < 13; i++)
+        {
+            var x = byM.GetValueOrDefault(i, 0);
+            var month = CultureInfo.InvariantCulture.DateTimeFormat.GetMonthName(i);
+            Console.WriteLine($"{month,13} {x,8} {new string('=', (int)(k * x))}");
+        }
+    }
+    else
+    {
+        Console.WriteLine();
+        foreach (var line in linesFiltered.Take(options.Limit))
+        {
+            Console.WriteLine(line);
         }
     }
 }
